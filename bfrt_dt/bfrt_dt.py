a01de97671dce886346629d6c0acd7a0e67d50eb
Suneet Kumar Singh <38260494+ersuneetsingh@users.noreply.github.com>
2024-02-25 22:43:07 -0500

    Update bfrt_dt.py

--- bfrt_dt/bfrt_dt.py
+++ bfrt_dt/bfrt_dt.py
@@ -1,5 +1,5 @@
  ################################################################################
- # Copyright 2023 INTRIG
+ # Copyright 2024 INTRIG
  #
  # Licensed under the Apache License, Version 2.0 (the "License");
  # you may not use this file except in compliance with the License.
